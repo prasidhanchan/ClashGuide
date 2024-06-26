befdf077335d983089be3bcf9e15b5cb87643fe6
Prasidh Gopal Anchan <prasidhgopalanchan@gmail.com>
2024-06-26 18:56:47 +0530

    chore: set window position and size

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,5 +1,10 @@
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import clashguide.composeapp.generated.resources.Res
 import clashguide.composeapp.generated.resources.app_name
 import di.KoinInitializer
@@ -7,10 +12,15 @@ import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     KoinInitializer().initKoin()
+    val state = rememberWindowState(
+        position = WindowPosition(Alignment.Center),
+        size = DpSize(width = 800.dp, height = 700.dp)
+    )
 
     Window(
         onCloseRequest = ::exitApplication,
-        title = stringResource(Res.string.app_name)
+        state = state,
+        title = stringResource(resource = Res.string.app_name)
     ) {
         ClashGuideApp()
     }
